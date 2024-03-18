9660cc8bbcf08cf0f974987e0eef812b30930fed
TV <tv@TVs-MacBook-Pro.local>
2024-03-18 15:49:39 +0530

    new func added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        add();
+    }
+
+    public static void add(){
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(4);
+
+        Stream<Integer> ans = nums.stream().sorted();
+
+        System.out.println(ans);
+
     }
 }
