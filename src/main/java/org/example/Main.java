df260c36c0efe86897142ab313b4d5015179596d
TV <tv@TVs-MacBook-Pro.local>
2024-03-18 16:05:38 +0530

    new function added in prac branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        list();
+    }
+
+    public static void list(){
+        List<Integer> li = new ArrayList<>();
+        li.add(23);
+        li.add(25);
+        li.add(34);
+        li.add(46);
+
+        Stream<Integer> n = li.stream().peek(System.out::println);
     }
 }
